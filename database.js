5e3c8a54603f20aa5a75e5a236b8619b6c618ecb
Yago Ramires <yago.ramiresx@gmail.com>
2026-01-09 18:42:53 -0300

    feat: update food items in database with new entries, categories, and adjusted prices

--- database.js
+++ database.js
@@ -494,102 +494,119 @@
 // ];
 
 export const foods = [
-  { id: 0, category: 'Entradas', name: 'Sunomono', unityPrice: 1 },
-  { id: 1, category: 'Entradas', name: 'Shimeji e Shitake', unityPrice: 1 },
-  { id: 2, category: 'Entradas', name: 'Shimeji', unityPrice: 1 },
-  { id: 3, category: 'Entradas', name: 'Shitake', unityPrice: 1 },
-  { id: 4, category: 'Entradas', name: 'Kakiague', unityPrice: 1 },
-  { id: 5, category: 'Entradas', name: 'Harumaki de Camarão', unityPrice: 1 },
-  { id: 6, category: 'Entradas', name: 'Camarão Empanado', unityPrice: 1 },
-  { id: 7, category: 'Entradas', name: 'Nirá', unityPrice: 1 },
-  { id: 8, category: 'Entradas', name: 'Guioza Suíno', unityPrice: 1 },
-  { id: 9, category: 'Entradas', name: 'Kakiague de Milho', unityPrice: 1 },
-  { id: 10, category: 'Entradas', name: 'Edamame', unityPrice: 1 },
-  { id: 11, category: 'Entradas', name: 'Harumaki de Legumes', unityPrice: 1 },
-  { id: 12, category: 'Entradas', name: 'Robata de Carne', unityPrice: 1 },
-  { id: 13, category: 'Entradas', name: 'Robata de Camarão', unityPrice: 1 },
-  { id: 14, category: 'Entradas', name: 'Robata de Lula', unityPrice: 1 },
-  { id: 15, category: 'Entradas', name: 'Isca de Tilápia', unityPrice: 1 },
-  { id: 16, category: 'Entradas', name: 'Casquinha de Salmão', unityPrice: 1 },
-  { id: 17, category: 'Entradas', name: 'Ika no Furai', unityPrice: 1 },
-  { id: 18, category: 'Entradas', name: 'Croqueta de Polvo', unityPrice: 1 },
-  { id: 19, category: 'Entradas', name: 'Dadinho de Tapioca', unityPrice: 1 },
-  { id: 20, category: 'Entradas', name: 'Bolinho de Salmão', unityPrice: 1 },
+  // ======================
+  // NOVIDADES RODÍZIO
+  // ======================
+  { id: 0, category: 'Novidades', name: 'Ceviche', unityPrice: 81.8 },
 
-  { id: 21, category: 'Marinados', name: 'Marinado Trufado', unityPrice: 1 },
-  { id: 22, category: 'Marinados', name: 'Sashimi Roasted no Alho', unityPrice: 1 },
-  { id: 23, category: 'Marinados', name: 'Marinado de Ervas', unityPrice: 1 },
-  { id: 24, category: 'Marinados', name: 'Marinado de Salmão com Alcaparras', unityPrice: 1 },
-  { id: 25, category: 'Marinados', name: 'Marinado de Polvo', unityPrice: 1 },
-  { id: 26, category: 'Marinados', name: 'Marinado Saint Peter', unityPrice: 1 },
+  { id: 1, category: 'Sushi', name: 'Sushi Salmão com Mel e Trufa', unityPrice: 11.95 }, // 23.90 / 2
+  { id: 2, category: 'Sushi', name: 'Sushi Atum com Teriyaki', unityPrice: 10.45 }, // 20.90 / 2
+  { id: 3, category: 'Sushi', name: 'Sushi Peixe Branco com Molho de Ostra', unityPrice: 9.95 }, // 19.90 / 2
 
-  { id: 27, category: 'Hot Rolls', name: 'Mini-Hot Couve', unityPrice: 1 },
-  { id: 28, category: 'Hot Rolls', name: 'Mini-Hot Spicy', unityPrice: 1 },
-  { id: 29, category: 'Hot Rolls', name: 'Hot Salmão Philadelphia', unityPrice: 1 },
-  { id: 30, category: 'Hot Rolls', name: 'Mauro Naves', unityPrice: 1 },
-  { id: 31, category: 'Hot Rolls', name: 'Mini-Hot Crispy', unityPrice: 1 },
-  { id: 32, category: 'Hot Rolls', name: 'Mini-Hot Rapadura', unityPrice: 1 },
-  { id: 33, category: 'Hot Rolls', name: 'Mini-Hot Camarão', unityPrice: 1 },
+  { id: 4, category: 'Hot Rolls', name: 'Mini Hot Chilli', unityPrice: 2.83 }, // 33.90 / 12
+  { id: 5, category: 'Marinados', name: 'Marinado Peixe Branco Cítrico', unityPrice: 64.0 },
 
-  { id: 34, category: 'Yakissoba', name: 'Carne e Frango', unityPrice: 1 },
-  { id: 35, category: 'Yakissoba', name: 'Camarão', unityPrice: 1 },
-  { id: 36, category: 'Yakissoba', name: 'Legumes', unityPrice: 1 },
-  { id: 37, category: 'Yakissoba', name: 'Frango', unityPrice: 1 },
-  { id: 38, category: 'Yakissoba', name: 'Carne', unityPrice: 1 },
+  // ======================
+  // ENTRADAS
+  // ======================
+  { id: 6, category: 'Entradas', name: 'Sunomono', unityPrice: 20.9 },
+  { id: 7, category: 'Entradas', name: 'Shimeji e Shitake', unityPrice: 38.9 },
+  { id: 8, category: 'Entradas', name: 'Shimeji', unityPrice: 38.9 },
+  { id: 9, category: 'Entradas', name: 'Shitake', unityPrice: 38.9 },
 
-  { id: 39, category: 'Teppan', name: 'Salmão', unityPrice: 1 },
+  { id: 10, category: 'Entradas', name: 'Kakiague', unityPrice: 3.8 }, // 19.00 / 5
+  { id: 11, category: 'Entradas', name: 'Harumaki de Camarão', unityPrice: 8.38 }, // 41.90 / 5
+  { id: 12, category: 'Entradas', name: 'Harumaki de Legumes', unityPrice: 4.98 }, // 24.90 / 5
+  { id: 13, category: 'Entradas', name: 'Guioza Suíno', unityPrice: 5.18 }, // 25.90 / 5
+  { id: 14, category: 'Entradas', name: 'Kakiague de Milho', unityPrice: 3.8 }, // 19.00 / 5
+  { id: 15, category: 'Entradas', name: 'Edamame', unityPrice: 29.9 },
+  { id: 16, category: 'Entradas', name: 'Nirá', unityPrice: 44.9 },
 
-  { id: 40, category: 'Sashimi', name: 'Atum', unityPrice: 1 },
-  { id: 41, category: 'Sashimi', name: 'Salmão', unityPrice: 1 },
-  { id: 42, category: 'Sashimi', name: 'Peixe Branco', unityPrice: 1 },
-  { id: 43, category: 'Sashimi', name: 'Polvo', unityPrice: 1 },
-  { id: 44, category: 'Sashimi', name: 'Haddock', unityPrice: 1 },
+  { id: 17, category: 'Entradas', name: 'Camarão Empanado', unityPrice: 9.78 }, // 48.90 / 5
+  { id: 18, category: 'Entradas', name: 'Isca de Tilápia', unityPrice: 6.98 }, // 34.90 / 5
+  { id: 19, category: 'Entradas', name: 'Casquinha de Salmão', unityPrice: 7.98 }, // estimado / 5
+  { id: 20, category: 'Entradas', name: 'Ika no Furai', unityPrice: 8.58 }, // 42.90 / 5
+  { id: 21, category: 'Entradas', name: 'Croqueta de Polvo', unityPrice: 5.98 }, // 29.90 / 5
+  { id: 22, category: 'Entradas', name: 'Dadinho de Tapioca', unityPrice: 6.2 }, // estimado / 5
+  { id: 23, category: 'Entradas', name: 'Bolinho de Salmão', unityPrice: 6.2 }, // estimado / 5
 
-  { id: 45, category: 'Sushi', name: 'Sushi Camarão', unityPrice: 1 },
-  { id: 46, category: 'Sushi', name: 'Sushi Salmão Trufado', unityPrice: 1 },
-  { id: 47, category: 'Sushi', name: 'Sushi Salmão', unityPrice: 1 },
-  { id: 48, category: 'Sushi', name: 'Sushi Salmão Selado no Alho', unityPrice: 1 },
-  { id: 49, category: 'Sushi', name: 'Sushi Salmão Skin', unityPrice: 1 },
-  { id: 50, category: 'Sushi', name: 'Sushi Atum', unityPrice: 1 },
-  { id: 51, category: 'Sushi', name: 'Sushi Peixe Branco', unityPrice: 1 },
-  { id: 52, category: 'Sushi', name: 'Sushi Skin Naves', unityPrice: 1 },
-  { id: 53, category: 'Sushi', name: 'Sushi de Saint Peter Maçaricado', unityPrice: 1 },
-  { id: 54, category: 'Sushi', name: 'Sushi de Polvo', unityPrice: 1 },
+  // ======================
+  // MARINADOS
+  // ======================
+  { id: 24, category: 'Marinados', name: 'Marinado Trufado', unityPrice: 69.2 },
+  { id: 25, category: 'Marinados', name: 'Sashimi Roasted no Alho', unityPrice: 5.03 }, // 50.30 / 10
+  { id: 26, category: 'Marinados', name: 'Marinado de Ervas', unityPrice: 64.0 },
+  { id: 27, category: 'Marinados', name: 'Marinado de Salmão com Alcaparras', unityPrice: 64.0 },
+  { id: 28, category: 'Marinados', name: 'Marinado de Polvo', unityPrice: 69.9 },
+  { id: 29, category: 'Marinados', name: 'Marinado Saint Peter', unityPrice: 69.9 },
 
-  { id: 55, category: 'Temaki', name: 'Salmão', unityPrice: 1 },
-  { id: 56, category: 'Temaki', name: 'Salmão Completo', unityPrice: 1 },
-  { id: 57, category: 'Temaki', name: 'Atum', unityPrice: 1 },
-  { id: 58, category: 'Temaki', name: 'Atum Completo', unityPrice: 1 },
-  { id: 59, category: 'Temaki', name: 'Temaki do Chef', unityPrice: 1 },
-  { id: 60, category: 'Temaki', name: 'Super-Hot', unityPrice: 1 },
+  // ======================
+  // HOT ROLLS
+  // ======================
+  { id: 30, category: 'Hot Rolls', name: 'Mini Hot Couve', unityPrice: 2.83 }, // 33.90 / 12
+  { id: 31, category: 'Hot Rolls', name: 'Mini Hot Spicy', unityPrice: 2.83 },
+  { id: 32, category: 'Hot Rolls', name: 'Mini Hot Crispy', unityPrice: 2.83 },
+  { id: 33, category: 'Hot Rolls', name: 'Mini Hot Rapadura', unityPrice: 2.83 },
+  { id: 34, category: 'Hot Rolls', name: 'Mini Hot Camarão', unityPrice: 2.83 },
+  { id: 35, category: 'Hot Rolls', name: 'Hot Salmão Philadelphia', unityPrice: 2.66 }, // 31.90 / 12
+  { id: 36, category: 'Hot Rolls', name: 'Mauro Naves', unityPrice: 3.41 }, // 40.90 / 12
 
-  { id: 61, category: 'Gunkas', name: 'Gunka Shakê Negui', unityPrice: 1 },
-  { id: 62, category: 'Gunkas', name: 'Gunka Crunchy', unityPrice: 1 },
-  { id: 63, category: 'Gunkas', name: 'Gunka Crispy', unityPrice: 1 },
-  { id: 64, category: 'Gunkas', name: 'Gunka Luka', unityPrice: 1 },
-  { id: 65, category: 'Gunkas', name: 'Gunka Massagô', unityPrice: 1 },
-  { id: 66, category: 'Gunkas', name: 'Gunka Rapadura', unityPrice: 1 },
-  { id: 67, category: 'Gunkas', name: 'Gunka Ebi Furai', unityPrice: 1 },
-  { id: 68, category: 'Gunkas', name: 'Gunka Couve', unityPrice: 1 },
-  { id: 69, category: 'Gunkas', name: 'Gunka Pistache', unityPrice: 1 },
-  { id: 70, category: 'Gunkas', name: 'Gunka Abacaxi', unityPrice: 1 },
+  // ======================
+  // SASHIMI
+  // ======================
+  { id: 37, category: 'Sashimi', name: 'Sashimi Salmão', unityPrice: 6.28 }, // 37.70 / 6
+  { id: 38, category: 'Sashimi', name: 'Sashimi Atum', unityPrice: 6.82 }, // 40.90 / 6
+  { id: 39, category: 'Sashimi', name: 'Sashimi Peixe Branco', unityPrice: 5.77 }, // 34.60 / 6
+  { id: 40, category: 'Sashimi', name: 'Sashimi Polvo', unityPrice: 6.65 }, // 39.90 / 6
+  { id: 41, category: 'Sashimi', name: 'Sashimi Haddock', unityPrice: 5.93 }, // 35.60 / 6
 
-  { id: 71, category: 'Rolls', name: 'Hossomaki Filadélfia', unityPrice: 1 },
-  { id: 72, category: 'Rolls', name: 'Uramaki Filadélfia', unityPrice: 1 },
-  { id: 73, category: 'Rolls', name: 'Uramaki Califórnia', unityPrice: 1 },
-  { id: 74, category: 'Rolls', name: 'Uramaki Ebiten', unityPrice: 1 },
-  { id: 75, category: 'Rolls', name: 'Uramaki Salmão', unityPrice: 1 },
-  { id: 76, category: 'Rolls', name: 'Uramaki Atum', unityPrice: 1 },
-  { id: 77, category: 'Rolls', name: 'Hossomaki Salmão', unityPrice: 1 },
-  { id: 78, category: 'Rolls', name: 'Hossomaki Atum', unityPrice: 1 },
-  { id: 79, category: 'Rolls', name: 'Uramaki de Salmão Especial', unityPrice: 1 },
+  // ======================
+  // SUSHI
+  // ======================
+  { id: 42, category: 'Sushi', name: 'Sushi Salmão', unityPrice: 10.95 }, // 21.90 / 2
+  { id: 43, category: 'Sushi', name: 'Sushi Salmão Trufado', unityPrice: 11.95 }, // 23.90 / 2
+  { id: 44, category: 'Sushi', name: 'Sushi Salmão Selado no Alho', unityPrice: 11.45 }, // 22.90 / 2
+  { id: 45, category: 'Sushi', name: 'Sushi Salmão Skin', unityPrice: 8.95 }, // 17.90 / 2
+  { id: 46, category: 'Sushi', name: 'Sushi Atum', unityPrice: 10.45 },
+  { id: 47, category: 'Sushi', name: 'Sushi Peixe Branco', unityPrice: 9.95 },
+  { id: 48, category: 'Sushi', name: 'Sushi Camarão', unityPrice: 10.95 },
+  { id: 49, category: 'Sushi', name: 'Sushi Skin Naves', unityPrice: 9.95 },
+  { id: 50, category: 'Sushi', name: 'Sushi Saint Peter Maçaricado', unityPrice: 10.95 },
+  { id: 51, category: 'Sushi', name: 'Sushi de Polvo', unityPrice: 16.45 },
 
-  { id: 80, category: 'Sobremesa', name: 'Bola de Sorvete', unityPrice: 1 },
-  { id: 81, category: 'Sobremesa', name: 'Hot Banana com Nutella', unityPrice: 1 },
-  { id: 82, category: 'Sobremesa', name: 'Mini Banana Flambada', unityPrice: 1 },
-  { id: 83, category: 'Sobremesa', name: 'Harumaki de Chocolate', unityPrice: 1 },
-  { id: 84, category: 'Sobremesa', name: 'Petit Gateau', unityPrice: 1 },
-  { id: 85, category: 'Sobremesa', name: 'Harumaki de Creme de Avelã', unityPrice: 1 },
-  { id: 86, category: 'Sobremesa', name: 'Brownie do Luiz com Sorvete', unityPrice: 1 },
-  { id: 87, category: 'Sobremesa', name: 'Harumaki de Chocolate Branco', unityPrice: 1 },
+  // ======================
+  // TEMAKI
+  // ======================
+  { id: 52, category: 'Temaki', name: 'Temaki Salmão', unityPrice: 29.3 },
+  { id: 53, category: 'Temaki', name: 'Temaki Salmão Completo', unityPrice: 39.8 },
+  { id: 54, category: 'Temaki Atum', unityPrice: 28.3 },
+  { id: 55, category: 'Temaki', name: 'Temaki Atum Completo', unityPrice: 35.6 },
+  { id: 56, category: 'Temaki', name: 'Temaki do Chef', unityPrice: 41.9 },
+
+  // ======================
+  // GUNKA
+  // ======================
+  { id: 57, category: 'Gunka', name: 'Gunka Shakê Negui', unityPrice: 4.98 }, // 19.90 / 4
+  { id: 58, category: 'Gunka', name: 'Gunka Crunchy', unityPrice: 4.45 },
+  { id: 59, category: 'Gunka', name: 'Gunka Crispy', unityPrice: 4.23 },
+  { id: 60, category: 'Gunka', name: 'Gunka Luka', unityPrice: 3.93 },
+  { id: 61, category: 'Gunka', name: 'Gunka Massagô', unityPrice: 4.98 },
+  { id: 62, category: 'Gunka', name: 'Gunka Rapadura', unityPrice: 4.73 },
+  { id: 63, category: 'Gunka', name: 'Gunka Ebi Furai', unityPrice: 4.98 },
+  { id: 64, category: 'Gunka', name: 'Gunka Couve', unityPrice: 4.45 },
+  { id: 65, category: 'Gunka', name: 'Gunka Pistache', unityPrice: 5.23 },
+  { id: 66, category: 'Gunka', name: 'Gunka Abacaxi', unityPrice: 4.23 },
+
+  // ======================
+  // ROLLS
+  // ======================
+  { id: 67, category: 'Rolls', name: 'Hossomaki Filadélfia', unityPrice: 3.74 }, // 29.90 / 8
+  { id: 68, category: 'Rolls', name: 'Uramaki Filadélfia', unityPrice: 3.74 },
+  { id: 69, category: 'Rolls', name: 'Uramaki Califórnia', unityPrice: 2.61 }, // 20.90 / 8
+  { id: 70, category: 'Rolls', name: 'Uramaki Ebiten', unityPrice: 4.71 }, // 37.70 / 8
+  { id: 71, category: 'Rolls', name: 'Uramaki Salmão', unityPrice: 3.54 }, // 28.30 / 8
+  { id: 72, category: 'Rolls', name: 'Uramaki Atum', unityPrice: 3.66 }, // 29.30 / 8
+  { id: 73, category: 'Rolls', name: 'Hossomaki Salmão', unityPrice: 1.83 }, // 14.60 / 8
+  { id: 74, category: 'Rolls', name: 'Hossomaki Atum', unityPrice: 2.35 }, // 18.80 / 8
+  { id: 75, category: 'Rolls', name: 'Uramaki de Salmão Especial', unityPrice: 3.99 }
 ];
